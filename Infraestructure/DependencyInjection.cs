9ca3c2d32984d948a0a9af0f479c56bbdf6d989f
Guillermo Valverde <guillermovalverde42@gmail.com>
2025-12-09 12:12:14 -0600

    add database connection settings and dependency injection for infrastructure services

---
+++ Infraestructure/DependencyInjection.cs
@@ -0,0 +1,37 @@
+using Application.Interfaces;
+using Infraestructure.Persistence;
+using Infraestructure.Scraper;
+using Infraestructure.Scraper.Core;
+using Infraestructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infraestructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        // 1. Base de Datos (Postgres)
+        var connectionString = configuration.GetConnectionString("DbConnection");
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+        // 2. Repositorios
+        // services.AddScoped<IProductRepository, ProductRepository>(); // (Cuando lo crees)
+
+        // 3. Scraping
+        services.AddSingleton<PlaywrightEngine>(); // Motor pesado compartido
+        services.AddTransient<ScraperFactory>();   // Fábrica ligera
+        
+        // Registramos las estrategias específicas
+        services.AddTransient<IScraperStrategy, AmazonStrategy>();
+        services.AddTransient<IScraperStrategy, MercadoLibreStrategy>();
+
+        // Registramos el servicio que usa la Application
+        services.AddScoped<IScraperService, ScraperService>();
+
+        return services;
+    }
+}
